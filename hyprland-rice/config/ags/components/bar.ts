4e62e532943089420bacfd0b23634c59d5f21d58
swomf <swomf@proton.me>
2024-06-01 17:08:48 -0400

    feat(hyprland-rice): sort workspaces in bar

--- hyprland-rice/config/ags/components/bar.ts
+++ hyprland-rice/config/ags/components/bar.ts
@@ -5,12 +5,15 @@ const systemtray = await Service.import("systemtray")
 
 function Workspaces() {
   const activeId = hyprland.active.workspace.bind("id")
+  // sort workspaces
   const workspaces = hyprland.bind("workspaces")
-    .as(ws => ws.map(({ id }) => Widget.Button({
-      on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-      child: Widget.Label(`${id}`),
-      class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
-    })))
+    .as(ws => ws
+      .sort((a, b) => a.id - b.id)
+      .map(({ id }) => Widget.Button({
+        on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
+        child: Widget.Label(`${id}`),
+        class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
+      })))
 
   return Widget.Box({
     class_name: "workspaces",
