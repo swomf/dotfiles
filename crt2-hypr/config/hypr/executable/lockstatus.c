85c753e934789a1ea9571a34ee9890ec9f475c58
swomf <swomf@proton.me>
2024-09-16 17:05:02 -0400

    crt2-hypr: lockstatus shows time and correct used ram
    Reduce hyprlock query time from 50 to 30 seconds

--- crt2-hypr/config/hypr/executable/lockstatus.c
+++ crt2-hypr/config/hypr/executable/lockstatus.c
@@ -4,9 +4,9 @@
 #include <string.h>
 #include <time.h>
 
-#define RECT_LEN 16
-#define DATE_LEN 11
-#define MEM_LEN 4 // e.g. 123.5 is too long but 841 isnt
+#define RECT_LEN 17
+#define DATE_LEN 18
+#define MEM_LEN 5 // e.g. 15.3
 #define BATTERY_DIR "/sys/class/power_supply/BAT1"
 
 typedef enum { kB, MiB, GiB, TiB } data_size;
@@ -48,14 +48,15 @@ int main() {
   char mem_str[RECT_LEN];
   char mem_used_str[MEM_LEN];
   char mem_total_str[MEM_LEN];
-  snprintf(mem_used_str, MEM_LEN, "%3.1f%s", mem.used,
+  snprintf(mem_used_str, MEM_LEN, "%4.1f%s", mem.used,
            data_size_str[mem.used_unit]);
-  snprintf(mem_total_str, MEM_LEN, "%3.1f%s", mem.total,
+  snprintf(mem_total_str, MEM_LEN, "%4.1f%s", mem.total,
            data_size_str[mem.total_unit]);
   snprintf(mem_str, RECT_LEN, "%s%s / %s%s", mem_used_str,
            data_size_str[mem.used_unit], mem_total_str,
            data_size_str[mem.total_unit]);
   printf("%*s\n", RECT_LEN, mem_str);
+  
 
   return 0;
 }
@@ -63,7 +64,7 @@ int main() {
 int update_date(char *date_str, size_t date_len) {
   time_t now = time(NULL);
   struct tm *tm = localtime(&now);
-  if (strftime(date_str, date_len, "%Y-%m-%d", tm) == 0) {
+  if (strftime(date_str, date_len, "%Y-%m-%d %H:%M", tm) == 0) {
     return -1;
   }
   return 0;
@@ -92,13 +93,15 @@ int update_memory(Mem *mem) {
   }
 
   char line[128];
+  float available;
   while (fgets(line, sizeof(line), fp) != NULL) {
     if (strncmp(line, "MemTotal:", 9) == 0) {
       sscanf(line, "MemTotal: %f kB", &mem->total);
-    } else if (strncmp(line, "Cached:", 7) == 0) {
-      sscanf(line, "Cached: %f kB", &mem->used);
+    } else if (strncmp(line, "MemAvailable:", 13) == 0) {
+      sscanf(line, "MemAvailable: %f kB", &available);
     }
   }
+  mem->used = mem->total - available;
   fclose(fp);
 
   while (mem->total >= 1000) {
