fe6b6d0055b5a26c29c0aa942d5df1786cbb1ab6
swomf <swomf@proton.me>
2026-02-01 17:57:16 -0500

    {nvim,zathura}: add synctex support
    I use zathura + latex-fast-compile + vimtex inverse search.

--- gentoo/config/nvim/lua/config/options.lua
+++ gentoo/config/nvim/lua/config/options.lua
@@ -3,3 +3,14 @@
 -- Add any additional options here
 vim.g.snacks_animate_scroll = false
 vim.opt.conceallevel = 0
+
+-- tabbing behavior
+-- vim.opt.expandtab = false
+-- vim.opt.tabstop = 4
+-- vim.opt.shiftwidth = 4
+-- vim.opt.softtabstop = 0
+
+-- synctex setup
+vim.g.vimtex_compiler_enabled = 0
+vim.g.vimtex_view_method = "zathura_simple"
+vim.g.vimtex_view_automatic = 1
