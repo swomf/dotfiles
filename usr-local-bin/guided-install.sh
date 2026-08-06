3b81a37266321091a4eccf1a139d26d1b62d37ad
swomf <swomf@proton.me>
2026-08-06 14:20:09 -0400

    usr-local-bin: scripts are shellcheck-compliant

--- usr-local-bin/guided-install.sh
+++ usr-local-bin/guided-install.sh
@@ -3,6 +3,11 @@
 SCRIPT_DIR="$(readlink -f "$(dirname "$(readlink -f "$0")")")"
 source "$SCRIPT_DIR/lib.sh"
 
-for f in $(find scripts -type f); do
-  confirm "$perm" install -Dm755 "$f" /usr/local/bin/"$(basename $f)"
+for f in scripts/*; do
+  if cmp -s "$f" /usr/local/bin/"$(basename "$f")"; then
+    printf "%b::%b Skipped%b reinstall of %b %s\n" \
+      "$b_CCIN" "$b_CWAR" "$b_CDEF" "$CDEF" "$f"
+  else
+    confirm "$perm" install -Dm755 "$f" /usr/local/bin/"$(basename "$f")"
+  fi
 done
--- usr-local-bin/lib.sh
+++ usr-local-bin/lib.sh
@@ -2,16 +2,36 @@
 
 export perm=sudo
 
+export CDEF="\033[0m"      # default color
+export CCIN="\033[0;36m"   # info color
+export CGSC="\033[0;32m"   # success color
+export CRER="\033[0;31m"   # error color
+export CWAR="\033[0;33m"   # warning color
+export b_CDEF="\033[1;37m" # bold default color
+export b_CCIN="\033[1;36m" # bold info color
+export b_CGSC="\033[1;32m" # bold success color
+export b_CRER="\033[1;31m" # bold error color
+export b_CWAR="\033[1;33m" # bold warning color
+
 confirm() {
-  printf '\033[1;36m::\033[1;37m Want to run \033[0m'
+  printf "%b::%b Want to run %b " "$b_CCIN" "$b_CDEF" "$CDEF"
   printf '%q ' "$@"
   printf '\nContinue?'
   read -r -p " [y/N] " reply
   case "$reply" in
   [Yy] | [Yy][Ee][Ss])
     "$@"
-    echo "   $@"
+    retcode=$?
+    check=
+    if ((retcode == 0)); then
+      check="$b_CGSC✓$CDEF"
+    else
+      check="$b_CRER✗$CDEF"
+    fi
+    printf '[%b] ' "$check"
+    printf "%q " "$@"
+    printf '\n'
     ;;
-  *) printf "\033[1;31mSkipped.\033[0m\n" ;;
+  *) printf "%bSkipped.%b\n" "$b_CWAR" "$CDEF" ;;
   esac
 }
